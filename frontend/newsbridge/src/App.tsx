4a8b4e972812160f5246a17859676adfe85ac58a
Kieran <kserra@umass.edu>
2025-03-06 09:17:39 -0500

    Initialize React-Ts Frontend, configure Tailwindcss, Add pages and components folders

---
+++ frontend/newsbridge/src/App.tsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-semibold text-2xl text-center">
+      NewsBridge
+    </div>
+  )
+}
+
+export default App
