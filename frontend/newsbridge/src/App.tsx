4422f81aab545a339942cbdc559d24001f06d9aa
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-11 20:12:25 -0400

    Fix file/directory naming conventions
    - Files should be PascalCase
    - Directories should be snake_case

--- frontend/newsbridge/src/App.tsx
+++ frontend/newsbridge/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import FormElementsPage from "./pages/component-testing-pages.tsx/FormElementsPage.tsx";
+import FormElementsPage from "./pages/component_testing_pages/FormElementsPage";
+
 
 function App() {
   return (
