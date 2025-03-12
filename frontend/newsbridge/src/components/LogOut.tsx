e9ad91562bad3ee7b61539cf023de7e29f8bcb61
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-12 14:25:55 -0400

    Make popup background opaque

--- frontend/newsbridge/src/components/LogOut.tsx
+++ frontend/newsbridge/src/components/LogOut.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaQuestionCircle } from 'react-icons/fa';
-import { Button } from './FormElements'; // Import Button component
+import { Button } from './FormElements';
 
 interface LogOutProps {
     onClose: () => void;
@@ -16,14 +16,15 @@ const LogOut: React.FC<LogOutProps> = ({ onClose }) => {
     };
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+        <div className="fixed inset-0 flex items-center justify-center">
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className="relative bg-white p-6 rounded-lg shadow-lg text-center">
                 <FaQuestionCircle className="w-12 h-12 mx-auto mb-4" />
                 <h2 className="text-xl mb-4">Are you leaving?</h2>
                 <div className="flex justify-center space-x-4">
                     <Button
                         value="Cancel"
-                        width={100} // Set width to a reasonably small number
+                        width={100}
                         bgColor="bg-gray-300"
                         borderColor="border-gray-300"
                         textColor="text-black"
@@ -31,7 +32,7 @@ const LogOut: React.FC<LogOutProps> = ({ onClose }) => {
                     />
                     <Button
                         value="Yes"
-                        width={100} // Set width to a reasonably small number
+                        width={100}
                         bgColor="bg-red-600"
                         borderColor="border-red-600"
                         textColor="text-white"
