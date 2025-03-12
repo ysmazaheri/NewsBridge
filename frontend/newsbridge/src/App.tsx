a9f4cd843799a58d967fcc4aa532f3ff46cec229
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-11 20:15:51 -0400

    Implement conditional navbar
    Establish simple data structure to store paths that should have navbars. Ensures that pages that should not have navbars (e.g. authentication, pages without content) do not by default.

--- frontend/newsbridge/src/App.tsx
+++ frontend/newsbridge/src/App.tsx
@@ -1,19 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import FormElementsPage from "./pages/component_testing_pages/FormElementsPage";
 
+const navbarRoutes = ["/", "/FormElementsPage"]; // Add paths here that should have the Navbar
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <ConditionalNavbar />
       <Routes>
         <Route path="/FormElementsPage" element={<FormElementsPage />} />
         {/* Add more routes here as needed */}
       </Routes>
     </Router>
   );
+}
+
+function ConditionalNavbar() {
+  const location = useLocation();
+  const showNavbar = navbarRoutes.includes(location.pathname);
+
+  return showNavbar ? <Navbar /> : null;
 }
 
 export default App;
