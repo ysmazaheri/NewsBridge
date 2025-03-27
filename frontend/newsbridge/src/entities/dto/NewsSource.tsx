8b163c5aa5ce2fa9e8b7637fdc29458f4d867ac8
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-26 20:27:07 -0400

    News Source DTO

--- frontend/newsbridge/src/entities/dto/NewsSource.tsx
+++ frontend/newsbridge/src/entities/dto/NewsSource.tsx
@@ -1,1 +1,6 @@
-export interface NewsSource { }
+export interface NewsSource {
+  source_id: number; // Primary key, auto-incremented
+  name: string; // Name of the news source
+  url: string; // URL of the news source
+  country?: string | null; // Country of the news source
+}
