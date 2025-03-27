da67f29b011cabd1493a2e9becae97b4a449a673
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-26 20:27:02 -0400

    Like DTO

--- frontend/newsbridge/src/entities/dto/Like.tsx
+++ frontend/newsbridge/src/entities/dto/Like.tsx
@@ -1,1 +1,6 @@
-export interface Like { }
+export interface Like {
+  like_id: number; // Primary key, auto-incremented
+  user_id: number; // Foreign key referencing users(user_id)
+  unbiased_article_id: number; // Foreign key referencing unbiased_articles(unbiased_article_id)
+  created_at: string; // Timestamp of when the like was created
+}
