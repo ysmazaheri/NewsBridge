cfc67b4ea6fe2b5e27525bc69f659fc9bfc2b747
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-26 20:18:21 -0400

    Bookmark DTO

--- frontend/newsbridge/src/entities/dto/Bookmark.tsx
+++ frontend/newsbridge/src/entities/dto/Bookmark.tsx
@@ -1,1 +1,6 @@
-export interface Bookmark { }
+export interface Bookmark {
+  bookmark_id: number; // Primary key, auto-incremented
+  user_id: number; // Foreign key referencing users(user_id)
+  unbiased_article_id: number; // Foreign key referencing unbiased_articles(unbiased_article_id)
+  created_at: string; // Timestamp of when the bookmark was created
+}
