d415f0bb184eee2ff8e7cda324a7fcef0320f515
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-28 14:38:22 -0400

    Fixed comment VM
    Currently it was mistakenly resembling "bias rating"

--- frontend/newsbridge/src/entities/viewmodels/CommentVM.tsx
+++ frontend/newsbridge/src/entities/viewmodels/CommentVM.tsx
@@ -1,17 +1,19 @@
-import { UserBiasRating } from "../dtos/UserBiasRatingDTO";
+import { Comment } from "../dtos/CommentDTO";
 
 export interface CommentViewModel {
   id: number;
   userId: number;
   unbiasedArticleId: number;
-  rating: number;
+  content: string;
+  parentCommendId?: number; // Optional, for threaded comments. Currently not supported.
   createdAt: string;
 }
 
-export const mapCommentToViewModel = (rating: UserBiasRating): CommentViewModel => ({
-  id: rating.user_bias_rating_id,
-  userId: rating.user_id,
-  unbiasedArticleId: rating.unbiased_article_id,
-  rating: rating.rating,
-  createdAt: rating.created_at,
+export const mapCommentToViewModel = (comment: Comment): CommentViewModel => ({
+  id: comment.comment_id,
+  userId: comment.user_id,
+  unbiasedArticleId: comment.unbiased_article_id,
+  content: comment.content,
+  parentCommendId: comment.parent_comment_id || undefined, // Use undefined if not present
+  createdAt: comment.created_at,
 });
