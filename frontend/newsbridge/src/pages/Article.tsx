87deca92dff63f28eaa30b1ed9348947846317c2
Kieran <kserra@umass.edu>
2025-03-28 15:45:53 -0400

    Update mock data to use VM instead of DTO
    - Use view model for mock data and will eventually make call to backend to fill in data such as sources and comments

--- frontend/newsbridge/src/pages/Article.tsx
+++ frontend/newsbridge/src/pages/Article.tsx
@@ -3,13 +3,14 @@ import { FaBookmark } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import { NewsBridgeBiasScale, UserBiasScale } from "../components/BiasScale";
 import { mockArticle } from "../mock-data/MockArticle";
-import { UnbiasedArticleViewModel, mapUnbiasedArticleToViewModel } from "../entities/viewmodels/UnbiasedArticleVM";
+import { UnbiasedArticleViewModel } from "../entities/viewmodels/UnbiasedArticleVM";
 const ArticlePage: React.FC = () => {
 
     const [article, setArticle] = useState<UnbiasedArticleViewModel | null>(null);
 
     useEffect(() => {
-        setArticle(mapUnbiasedArticleToViewModel(mockArticle));
+        //Will be replaced with a call to the backend to get the article
+        setArticle(mockArticle);
     }, []);
 
     const handleBiasUpdate = (rating: number) => {
@@ -76,7 +77,7 @@ const ArticlePage: React.FC = () => {
                 {/* Audience Rating */}
                 <div className="w-full">
                     <h3 className="text-lg font-medium pb-3 text-center">Audience Bias Rating</h3>
-                    <NewsBridgeBiasScale rating={80} />
+                    <NewsBridgeBiasScale rating={article.audienceBiasRating} />
                 </div>
                 {/* User Rating */}
                 <div className="w-full">
