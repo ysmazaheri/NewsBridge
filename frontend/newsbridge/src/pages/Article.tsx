60a87745353c257ffb03f8c6838e99abc6bdca71
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-28 15:15:00 -0400

    Relocate mocked comments
    Moved mock data to designated mock folder

--- frontend/newsbridge/src/pages/Article.tsx
+++ frontend/newsbridge/src/pages/Article.tsx
@@ -5,39 +5,9 @@ import { NewsBridgeBiasScale, UserBiasScale } from "../components/BiasScale";
 import { mockArticle } from "../mock-data/MockArticle";
 import { UnbiasedArticleViewModel, mapUnbiasedArticleToViewModel } from "../entities/viewmodels/UnbiasedArticleVM";
 import CommentSection from "../components/CommentSection/CommentSection";
-import { CommentViewModel } from "../entities/viewmodels/CommentVM";
+import { mockComments } from "../mock-data/MockComments";
 
 const ArticlePage: React.FC = () => {
-    
-    //TODO: Move to mock data section
-    const mockComments: CommentViewModel[] = [
-        {
-            id: 1,
-            user: {
-                id: 1,
-                fullName: "User1",
-                email: "user1@example.com",
-                profilePicUrl: "/person.svg",
-                role: "user",
-                createdAt: "2023-01-01T12:00:00Z",
-            },
-            content: "This is the first comment.",
-            createdAt: "2023-01-01T12:00:00Z",
-        },
-        {
-            id: 2,
-            user: {
-                id: 2,
-                fullName: "User2",
-                email: "user2@example.com",
-                profilePicUrl: "/person.svg",
-                role: "user",
-                createdAt: "2023-01-02T12:00:00Z",
-            },
-            content: "This is another comment.",
-            createdAt: "2023-01-02T12:00:00Z",
-        },
-    ];
 
     const [article, setArticle] = useState<UnbiasedArticleViewModel | null>(null);
 
