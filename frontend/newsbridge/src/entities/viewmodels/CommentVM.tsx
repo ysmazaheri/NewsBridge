2cec2714f7d4bd24dc5a0524fd4d6764e906e5bb
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-28 14:49:37 -0400

    Make comment VM store user, not user ID

--- frontend/newsbridge/src/entities/viewmodels/CommentVM.tsx
+++ frontend/newsbridge/src/entities/viewmodels/CommentVM.tsx
@@ -1,9 +1,9 @@
 import { Comment } from "../dtos/CommentDTO";
+import { UserViewModel } from "./UserVM";
 
 export interface CommentViewModel {
   id: number;
-  userId: number;
-  unbiasedArticleId: number;
+  user: UserViewModel | null; //TODO: Remove acceptance of null as an option here
   content: string;
   parentCommendId?: number; // Optional, for threaded comments. Currently not supported.
   createdAt: string;
@@ -11,8 +11,7 @@ export interface CommentViewModel {
 
 export const mapCommentToViewModel = (comment: Comment): CommentViewModel => ({
   id: comment.comment_id,
-  userId: comment.user_id,
-  unbiasedArticleId: comment.unbiased_article_id,
+  user: null, //TODO: Get the user using a database call
   content: comment.content,
   parentCommendId: comment.parent_comment_id || undefined, // Use undefined if not present
   createdAt: comment.created_at,
