d1b012041e49cbf70299528451b8f980702fbb28
Kieran <kserra@umass.edu>
2025-03-28 20:39:54 -0400

    Add Sources Component
    - Created dropdown sources component where user can click on and it will display all sources associated with article
    - Currently working with mock sources data that will be replaced with real data from backend
    - Displays the first letter of each source and a + {number of remaining sources} if there is more than 3 sources

---
+++ frontend/newsbridge/src/mock-data/MockSources.tsx
@@ -0,0 +1,7 @@
+export const mockSources = [
+    {name: "BBC News", url: "https://www.bbc.com"},
+    {name: "CNN", url: "https://www.cnn.com"},
+    {name: "Fox News", url: "https://www.foxnews.com"},
+    {name: "MSNBC", url: "https://www.msnbc.com"},
+    {name: "The New York Times With A longer name", url: "https://www.nytimes.com"},
+]
