85e2cb6999c2d0324b5da7fd15b1f29dc900655d
Kevin Li <kevinlouisli7@gmail.com>
2025-03-29 16:38:24 -0400

    Bug fix, forgot to add navbar to profile page

--- frontend/newsbridge/src/App.tsx
+++ frontend/newsbridge/src/App.tsx
@@ -14,7 +14,7 @@ import BiasScalePage from "./pages/component_testing_pages/BiasScalePage";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 
-const navbarRoutes = ["/", "/form-elements-page", "/bias-scale-page"]; // Add paths here that should have the Navbar
+const navbarRoutes = ["/", "/form-elements-page", "/bias-scale-page", "/profile"]; // Add paths here that should have the Navbar
 
 function App() {
   return (
