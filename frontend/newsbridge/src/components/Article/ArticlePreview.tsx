02354559d6d9da707b16355a9fdd41377a9ef3e2
Bao Nguyen <nnbao204@gmail.com>
2025-03-29 02:46:17 -0400

    Added Home page for component testing
    - Added Home page for component testing
    - Connected to button in the NavBar
    - Created file for Article Preview Component

---
+++ frontend/newsbridge/src/components/Article/ArticlePreview.tsx
@@ -0,0 +1,7 @@
+const ArticlePreview = () => {
+    return (
+        <div>Hello2</div>
+    )
+}
+
+export default ArticlePreview;
---
+++ frontend/newsbridge/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import ArticlePreview from "../components/Article/ArticlePreview"
+
+const Home = () => {
+    return (
+        <ArticlePreview />
+    );
+}
+
+export default Home;
