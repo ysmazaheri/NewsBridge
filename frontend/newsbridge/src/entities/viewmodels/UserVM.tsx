3b92b5d1259cf7c99dac49e9b3136c1a319c2091
Kevin Li <kevinlouisli7@gmail.com>
2025-03-29 14:54:24 -0400

    Small bugfix in userVM

--- frontend/newsbridge/src/entities/viewmodels/UserVM.tsx
+++ frontend/newsbridge/src/entities/viewmodels/UserVM.tsx
@@ -14,7 +14,7 @@ export interface UserViewModel {
 export const mapUserToViewModel = (user: User): UserViewModel => ({
   id: user.user_id,
   firstName: user.first_name,
-  lastName: user.first_name,
+  lastName: user.last_name,
   fullName: `${user.first_name} ${user.last_name}`,
   email: user.email,
   profilePicUrl: user.profile_pic ? `/images/${user.profile_pic}` : null,
