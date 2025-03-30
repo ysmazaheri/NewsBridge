3e8d3584f1bbb3d38624c1f7a5d4458f10737160
Said Mazaheri <ysmazaheri@gmail.com>
2025-03-30 19:06:54 -0400

    Trending, bookmarked, search use properly mocked articles

--- frontend/newsbridge/src/pages/Search.tsx
+++ frontend/newsbridge/src/pages/Search.tsx
@@ -1,89 +1,9 @@
 import ArticleList from "../components/Article/ArticleList";
 import SearchComponent from "../components/Search/SearchComponent";
 import { UnbiasedArticlePreviewViewModel } from "../entities/viewmodels/UnbiasedArticlePreviewVM";
+import { mockArticlePreviews } from "../mock-data/MockArticlePreviews";
 
-const mockArticles: UnbiasedArticlePreviewViewModel[] = [
-    {
-      id: 1,
-      title: "This is a Title For a News Story",
-      summary:
-        "This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page.",
-      imageUrl: "/",
-      topic: "US News",
-      likeCount: 10,
-      genre: "News",
-      createdAt: "2025-10-10",
-      daysAgo: "3 days ago",
-      isBookmarked: true,
-      isLiked: false,
-      audienceBiasRating: 50,
-      commentCount: 13,
-    },
-    {
-      id: 2,
-      title: "This is a Title For a News Story",
-      summary:
-        "This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page.",
-      imageUrl: "/",
-      topic: "US News",
-      likeCount: 10,
-      genre: "News",
-      createdAt: "2025-10-10",
-      daysAgo: "3 days ago",
-      isBookmarked: false,
-      isLiked: false,
-      audienceBiasRating: 50,
-      commentCount: 19,
-    },
-    {
-      id: 3,
-      title: "This is a Title For a News Story",
-      summary:
-        "This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page.",
-      imageUrl: "/",
-      topic: "US News",
-      likeCount: 10,
-      genre: "News",
-      createdAt: "2025-10-10",
-      daysAgo: "3 days ago",
-      isBookmarked: true,
-      isLiked: false,
-      audienceBiasRating: 50,
-      commentCount: 22,
-    },
-    {
-      id: 4,
-      title: "This is a Title For a News Story",
-      summary:
-        "This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page.",
-      imageUrl: "/",
-      topic: "US News",
-      likeCount: 10,
-      genre: "News",
-      createdAt: "2025-10-10",
-      daysAgo: "3 days ago",
-      isBookmarked: false,
-      isLiked: false,
-      audienceBiasRating: 50,
-      commentCount: 10,
-    },
-    {
-      id: 5,
-      title: "This is a Title For a News Story",
-      summary:
-        "This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page. This is a summary of a news article that is being displayed on the home page.",
-      imageUrl: "/",
-      topic: "US News",
-      likeCount: 10,
-      genre: "News",
-      createdAt: "2025-10-10",
-      daysAgo: "3 days ago",
-      isBookmarked: true,
-      isLiked: false,
-      audienceBiasRating: 50,
-      commentCount: 10,
-    },
-  ];
+const mockArticles: UnbiasedArticlePreviewViewModel[] = mockArticlePreviews;
 
 const Search = () => { 
     return (
